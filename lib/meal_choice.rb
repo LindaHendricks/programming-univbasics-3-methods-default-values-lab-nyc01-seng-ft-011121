e7571a054eca99bfbd264d970a74bd8ea705612e
Linda-Monique Nyangoh Timoh Hendricks <linda.timoh@gmail.com>
2020-12-01 18:01:01 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,5 +3,5 @@ def meal_choice ( veg1, veg2, protein = "tofu" )
   return "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
-meal_choice ('carrots', 'string beans', 'lentils')
+puts meal_choice ('carrots', 'string beans', 'lentils')
 
